c3ffe98181630c1fdea6f21acf58dba0eabdbd32
Momin <mahvash-nazir.momin@capgemini.com>
2019-05-17 17:11:22 +0530

    files

---
+++ BeerSong.java
@@ -0,0 +1,24 @@
+public class BeerSong {
+    public static void main(String [] args) {
+        int beerNum=99;
+        String word= "bottles";
+        
+        while(beerNum > 0) {
+            if(beerNum==1) {
+                word= "bottle";
+            }
+            System.out.print(beerNum + " " + word + "of beer on the wall");
+            System.out.print(beerNum + " " + word + "of beer.");
+            System.out.print("Take one down");
+            System.out.print("Pass it around");
+            beerNum=beerNum-1;
+            
+            if(beerNum>0) {
+                System.out.print(beerNum + " " + word + "of beer on the wall");
+            }
+            else {
+                System.out.print("No more bottles of beer on the wall");
+            }
+        }
+    }
+}
